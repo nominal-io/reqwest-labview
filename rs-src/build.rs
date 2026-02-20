a7276fe80c02b28afca5450ab6324d3b8f2816f1
Tom <tom.mcquillan@nominal.io>
2026-02-20 10:35:28 +0000

    removed with_no_included

--- rs-src/build.rs
+++ rs-src/build.rs
@@ -6,9 +6,8 @@ fn main() {
         .with_crate(crate_dir)
         .with_language(cbindgen::Language::C)
         .with_include_guard("HTTP_RS_LABVIEW_H")
-        .with_std_types(true)           // Pulls in <stdint.h> types
-        .with_documentation(true)       // Includes doc comments as C comments
-        .with_no_includes(false)
+        .with_std_types(true)       // Emits uint32_t, int32_t etc. from <stdint.h>
+        .with_documentation(true)   // Includes doc comments as C comments
         .with_sys_include("stdint.h")
         .generate()
         .expect("Unable to generate C bindings")
